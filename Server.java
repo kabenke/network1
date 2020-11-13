ffa2e763065c838753ccd210061cd9a8910ee767
oyoki <kabenke_nana@yahoo.fr>
2020-11-13 22:37:36 +0100

    Server bekommt eine Nachricht von Client und  schickt  eine Antwort

--- Server.java
+++ Server.java
@@ -12,14 +12,26 @@ public class Server
 
             Socket s = ss.accept();     //le serveur attends une connexion
 
-            System.out.println("clien connected");
+            System.out.println("client connected");
 
             InputStreamReader in = new InputStreamReader(s.getInputStream());
 
             BufferedReader bf = new BufferedReader(in);
 
             String str = bf.readLine();
+                                           //si le client envoie un message
+            if (str != null) {
+
             System.out.println("message du client " + str);
+
+            System.out.println("J'envoie une reponse au client");
+
+            PrintWriter pr = new PrintWriter(s.getOutputStream());
+            pr.println("Nous avons recu votre message et vous aurez une reponse d'ici bientot");
+            pr.flush();
+            }
+            else{System.exit(0);}//si non je sors
+
         } catch (Exception e) {
             //TODO: handle exception
             e.printStackTrace();
